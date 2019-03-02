229311cf3d6cb629dc3e2186ceda333aaee81421
emiliska <33560517+emiliska@users.noreply.github.com>
2019-03-02 15:41:50 -0500

    simple script to help generate a group name for our space

--- group_name_generator.ps1
+++ group_name_generator.ps1
@@ -10,16 +10,14 @@
 # take in the textfile as a pipeline object from the command line
 # ex: ./group_name_generator.ps1 .\words.txt
 Param(
-    [parameter(Mandatory=$true,
-    ValueFromPipeline=$true)]
+    [Parameter(Mandatory=$true,
+    ValueFromPipeline=$true,
+    Position=0)]
     [String[]]
     $textFile
 )
 
 $word_array = Get-Content $textFile
-foreach ($word in $word_array) {
-    Write-Output $word
-}
 
 # +---------------------+
 # | generate a new word |
@@ -33,4 +31,36 @@ foreach ($word in $word_array) {
 # +-----------------------+
 # randomly pair the words up
 
-# print out a list of phrases
+while ($true){ # start loop
+#reset variables
+$newPhrase = ""
+
+# pick 2 poss in the array and save them
+$word_array_length = $word_array.Length
+$posA = Get-Random -Minimum 0 -Maximum $word_array_length
+$posB = Get-Random -Minimum 0 -Maximum $word_array_length
+
+# pack a word in the array and save it
+$countVar = 0
+foreach ($word in $word_array) {
+    if ($countVar -eq $posA -or $countVar -eq $posB){
+        $newPhrase = $newPhrase + $word + " "
+    }
+    $countVar++
+}
+
+# save to working file
+Out-File -FilePath .\newPhrases.txt -InputObject $newPhrase -Encoding ascii -Width 80 -Append
+#print out the new phrase
+Write-Output $newPhrase
+
+
+# +----------------------------------+
+# | get another phrase with "anykey" |
+# +----------------------------------+
+$userInput = Read-Host -Prompt "Press 'Enter' to continue or '0' to exit the program" # 'q', 'n', or 'esc' 
+# +----------------------------------------+
+# | exit the program with 'q' 'n' or 'esc' |
+# +----------------------------------------+
+if ($userInput -eq 0){Break}
+}
